1e6b5340bbc644b56e0c7415204434b8bf61f4e0
argg2159 <argg2159@berkeley.edu>
2019-04-29 21:04:10 -0700

    bin_cum: added plot method for class bincum

--- binomir/R/bin_cumulative.R
+++ binomir/R/bin_cumulative.R
@@ -23,6 +23,6 @@ bin_cumulative <- function(trials = 2, prob = .5){
 #' @export
 plot.bincum <- function(x, ...){
   ggplot2::ggplot(x, ggplot2::aes(x = success, y = cumulative)) +
-    ggplot2::geom_point() +
+    ggplot2::geom_point(shape = 1) +
     ggplot2::theme_classic()
 }
