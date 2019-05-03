3604f129d5f241874182e7c29d785bb693253053
argg2159 <argg2159@berkeley.edu>
2019-05-03 13:09:12 -0700

    devtools_flow: change default to build vignettes

--- binomial/devtools_flow.R
+++ binomial/devtools_flow.R
@@ -2,11 +2,12 @@
 #requires devtools!
 
 
-# in order to install vignettes use devtools::install(build_vignettes = TRUE)
+# if you don't need vignettes in index run with build_vignettes = FALSE
+# in devtools::install()
 devtools::document()
 devtools::check_man()
 devtools::test()
 devtools::build_vignettes()
 devtools::build()
-devtools::install()
+devtools::install(build_vignettes = TRUE)
 .rs.restartR()
