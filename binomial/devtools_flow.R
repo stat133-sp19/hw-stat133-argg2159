6d067c6d34a8eeecd65c3c0619a394748a8c90ad
argg2159 <argg2159@berkeley.edu>
2019-05-03 12:31:50 -0700

    devtools_flow: add small title

--- binomial/devtools_flow.R
+++ binomial/devtools_flow.R
@@ -1,8 +1,12 @@
+#Devtools Workflow:
+#requires devtools!
+
+
 # in order to install vignettes use devtools::install(build_vignettes = TRUE)
 devtools::document()
 devtools::check_man()
 devtools::test()
 devtools::build_vignettes()
 devtools::build()
-devtools::install(build_vignettes = TRUE)
+devtools::install()
 .rs.restartR()
